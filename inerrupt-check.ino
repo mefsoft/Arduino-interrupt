b80cc09d2c25b36ee74a821e9dab9c43c94db071
muhammet furkan çiftci <mfc_923@hotmail.com>
2018-07-12 22:51:16 +0300

    Add files via upload

---
+++ inerrupt-check.ino
@@ -0,0 +1,23 @@
+/*Powered By MEFSOFT*/
+/*You can use you can use this program, but the publishing rights are reserved by MEFSOFT*/
+/*Unauthorized publishing and sale of the program is prohibited.*/
+/* V-1.1*/
+
+int timer=0;
+void setup() {
+  //interrupt ayarlama
+  pinMode(2,INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(2), kesme, RISING);
+  Serial.begin(9600);
+}
+
+ void loop(){
+  delay(1000);
+ timer+=1;
+ }
+//interrupt fonksiyonu
+void kesme()
+{
+  Serial.println(timer);
+}
